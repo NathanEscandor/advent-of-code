dc37b6c0fcd8c29f3c1809e213841a66b89727c4
Nathan Escandor <nathan.escandor@gmail.com>
2021-12-01 16:42:13 -0700

    DAY-1 part 2 solution in place

--- Day_1/day1.py
+++ Day_1/day1.py
@@ -4,6 +4,15 @@ def get_depth_measurements():
         stripped = [int(s.strip()) for s in lines]
         return stripped
 
+def get_window_sums(window_size, measurements):
+    windows = []
+    for index in range(len(measurements)+1):
+        if index+window_size <= len(measurements):
+            window_sum = sum(measurements[index:index+window_size])
+            windows.append(window_sum)
+    return windows
+
+
 
 def part_1(measurements):
     prev = None
@@ -17,8 +26,13 @@ def part_1(measurements):
 
     return count
 
+def part_2():
+    pass
 
 measurements = get_depth_measurements()
 increases = part_1(measurements)
+windows = get_window_sums(3, measurements)
+window_increases = part_1(windows)
 
-print()
+breakpoint()
+# print()
