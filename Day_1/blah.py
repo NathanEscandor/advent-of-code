d41561f203e14be84eb2b0f33841a01cc3fa44db
Nathan Escandor <nathan.escandor@gmail.com>
2021-12-01 16:51:10 -0700

    DAY-1 some more cleanup

--- Day_1/blah.py
+++
@@ -1,9 +0,0 @@
-def get_input():
-    with open("puzzle_input.txt", "r") as f:
-        return [int(line.strip()) for line in f]
-
-def part_1(depth):
-    return sum([1 for i in range(len(depth)) if depth[i] > depth[i-1]])
-
-input = get_input()
-part_1(input)
