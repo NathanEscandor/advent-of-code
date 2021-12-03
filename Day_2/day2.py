557caad150759e10ada194463e6f1873b17ef99c
Nathan Escandor <nathan.escandor@gmail.com>
2021-12-02 20:13:25 -0700

    DAY-2 overwrote previous with second part
    second part commands overwrote what the original ones did

--- Day_2/day2.py
+++ Day_2/day2.py
@@ -4,41 +4,9 @@ def get_commands():
         stripped = [s.strip() for s in lines]
         return lines
 
-# def get_window_sums(measurements, window_size):
-#     windows = []
-#     for index in range(len(measurements)+1):
-#         if index+window_size <= len(measurements):
-#             window_sum = sum(measurements[index:index+window_size])
-#             windows.append(window_sum)
-#     return windows
-
-# def get_increases(measurements):
-#     prev = None
-#     count = 0
-#     for depth_measurement in measurements:
-#         if prev != None:
-#             if depth_measurement > prev:
-#                 count += 1
-
-#         prev = depth_measurement
-
-#     return count    
-
-
-# def part_1():
-#     measurements = get_depth_measurements()
-#     increases = get_increases(measurements)
-#     return increases
-
-# def part_2(window_size):
-#     measurements = get_depth_measurements()
-#     window_sums = get_window_sums(measurements, window_size)
-#     window_increases = get_increases(window_sums)
-#     return window_increases
-
 list_of_commands = get_commands()
 
-
+aim = 0
 position = 0
 depth = 0
 
@@ -49,11 +17,11 @@ for command in list_of_commands:
 
     if command_split[0] == 'forward':
         position = position + int(command_split[1])
+        depth = depth + (aim * int(command_split[1]))
     elif command_split[0] == 'down':
-        depth = depth + int(command_split[1])
+        aim = aim + int(command_split[1])
     elif command_split[0] == 'up':
-        depth = depth - int(command_split[1])
+        aim = aim - int(command_split[1])
 
-print(position)
-print(depth)
+print(position * depth)
 breakpoint()
