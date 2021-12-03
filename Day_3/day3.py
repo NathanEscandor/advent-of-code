56a29deee0523cfab6df9fcb2dffbd1a5c43d8a5
Nathan Escandor <nathan.escandor@gmail.com>
2021-12-02 23:19:10 -0700

    DAY-3 part 2 solution in place

--- Day_3/day3.py
+++ Day_3/day3.py
@@ -23,16 +23,73 @@ def get_gamma(lines):
         else:
             gamma.append(1)
     return gamma
-        
+
+def get_oxygen(lines):
+    position = 0
+    while len(lines) > 1:
+        zero = 0
+        one = 0
+        for line in lines:
+            if int(line[position]) == 1:
+                one += 1
+            elif int(line[position]) == 0:
+                zero += 1
+
+        newlist = []
+        if zero > one:
+            for line in lines:
+                if int(line[position]) == 0:
+                    newlist.append(line)
+        else:
+            for line in lines:
+                if int(line[position]) == 1:
+                    newlist.append(line)
+        lines = newlist
+        position += 1
+    
+    return lines
+
+
+def get_carbon(lines):
+    position = 0
+    while len(lines) > 1:
+        zero = 0
+        one = 0
+        for line in lines:
+            if int(line[position]) == 1:
+                one += 1
+            elif int(line[position]) == 0:
+                zero += 1
+
+        newlist = []
+        if zero <= one:
+            for line in lines:
+                if int(line[position]) == 0:
+                    newlist.append(line)
+        else:
+            for line in lines:
+                if int(line[position]) == 1:
+                    newlist.append(line)
+        lines = newlist
+        position += 1
+    
+    return lines
+
 report_lines = get_diagnostic()
-gamma = get_gamma(report_lines)
-gamma_str = ''.join(map(str, gamma))
-
-epsilon = ''.join(['1' if i == '0' else '0' for i in gamma_str])
-breakpoint()
-power_consumption = int(gamma_str, 2) * int(epsilon, 2)
+oxygen = int(get_oxygen(report_lines)[0], 2)
+carbon = int(get_carbon(report_lines)[0], 2)
 
 breakpoint()
+
+# gamma = get_gamma(report_lines)
+# gamma_str = ''.join(map(str, gamma))
+
+# epsilon = ''.join(['1' if i == '0' else '0' for i in gamma_str])
+# breakpoint()
+# power_consumption = int(gamma_str, 2) * int(epsilon, 2)
+
+
+
 
 
 
@@ -48,6 +105,3 @@ breakpoint()
 #         aim = aim + int(command_split[1])
 #     elif command_split[0] == 'up':
 #         aim = aim - int(command_split[1])
-
-print(position * depth)
-breakpoint()
